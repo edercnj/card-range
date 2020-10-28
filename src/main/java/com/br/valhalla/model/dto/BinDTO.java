856ef1223e7d465b08b98f70ae64907ff46042b0
Eder Celeste Nunes Júnior <edercnj@gmail.com>
2020-10-28 20:07:04 -0300

    implementation add update and delete endpoints

---
+++ src/main/java/com/br/valhalla/model/dto/BinDTO.java
@@ -0,0 +1,13 @@
+package com.br.valhalla.model.dto;
+
+import com.br.valhalla.model.entity.Issuer;
+import com.br.valhalla.model.enumeration.Product;
+
+public class BinDTO {
+
+    private int bin;
+    private String name;
+    private Issuer issuer;
+    private int binLenght;
+    private Product product;
+}
--- src/main/java/com/br/valhalla/resource/CardRangeResource.java
+++ src/main/java/com/br/valhalla/resource/CardRangeResource.java
@@ -1,5 +1,6 @@
 package com.br.valhalla.resource;
 
+import com.br.valhalla.model.dto.BinDTO;
 import com.br.valhalla.model.entity.Bin;
 
 import javax.ws.rs.*;
@@ -8,12 +9,27 @@ import javax.ws.rs.core.MediaType;
 @Path("/card-range")
 public class CardRangeResource {
 
-    @GET()
+    @GET
+    @Path("all")
     @Produces(MediaType.APPLICATION_JSON)
     public String getAll() {
         return "hello";
     }
     @POST
+    @Path("add")
     public void add(Bin bin)
     {}
+
+    @DELETE
+    @Path("delete/{id}")
+    public void delete(@PathParam("id") Long id)
+    {}
+
+    @PUT
+    @Path("update/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Bin update(@PathParam("id") Long id, BinDTO binDTO)
+    {
+        return null;
+    }
 }
