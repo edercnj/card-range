5b0d4f14c40990c9f747225313d21f024844fc30
Eder Celeste Nunes Júnior <edercnj@gmail.com>
2020-10-28 22:56:21 -0300

    add getters and setters and lombok dependecy

--- src/main/java/com/br/valhalla/cardrange/model/dto/BinDTO.java
+++ src/main/java/com/br/valhalla/cardrange/model/dto/BinDTO.java
@@ -2,12 +2,24 @@ package com.br.valhalla.cardrange.model.dto;
 
 import com.br.valhalla.cardrange.model.entity.Issuer;
 import com.br.valhalla.cardrange.model.enumeration.Product;
+import lombok.Getter;
+import lombok.Setter;
 
 public class BinDTO {
 
+    @Getter
+    @Setter
     private int bin;
+    @Getter
+    @Setter
     private String name;
+    @Getter
+    @Setter
     private Issuer issuer;
-    private int binLenght;
+    @Getter
+    @Setter
+    private int binLength;
+    @Getter
+    @Setter
     private Product product;
 }
--- src/main/java/com/br/valhalla/cardrange/model/entity/Bin.java
+++ src/main/java/com/br/valhalla/cardrange/model/entity/Bin.java
@@ -1,12 +1,24 @@
 package com.br.valhalla.cardrange.model.entity;
 
 import com.br.valhalla.cardrange.model.enumeration.Product;
+import lombok.Getter;
+import lombok.Setter;
 
 public class Bin {
-
+    @Getter
+    @Setter
     private int bin;
+    @Getter
+    @Setter
     private String name;
+    @Getter
+    @Setter
     private Issuer issuer;
-    private int binLenght;
+    @Getter
+    @Setter
+    private int binLength;
+    @Getter
+    @Setter
     private Product product;
+
 }
